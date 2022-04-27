5649b2cd8edc9259c5fa35b42e7c2d252ca035b9
Betinsk <gmdust@hotmail.com>
2022-04-27 13:17:15 -0300

    Clear Interval

--- jogo.js
+++ jogo.js
@@ -3,6 +3,7 @@
 var altura = 0	
 var largura = 0
 var vidas = 1
+var tempo = 7
 
 //Definindo tamanho da janela do navegador
 function ajustaTamanhoPalcoJogo() { 
@@ -13,6 +14,18 @@ largura = window.innerWidth
 
 ajustaTamanhoPalcoJogo()
 
+	var cronometro = setInterval(function() {
+		tempo -= 1
+		if(tempo < 0) {
+			clearInterval(cronometro)
+			clearInterval(criaMosquito)
+			alert('Vitoria')
+		}
+		else {
+		document.getElementById('cronometro').innerHTML = tempo
+		}
+	}, 1000)
+	
 
 function posicaoRandomica() {
 
