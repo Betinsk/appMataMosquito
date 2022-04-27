951e735e500a07e26411a7046c64a771c36fbff6
Betinsk <gmdust@hotmail.com>
2022-04-27 12:44:38 -0300

    Pagina game over

--- jogo.js
+++ jogo.js
@@ -21,7 +21,7 @@ function posicaoRandomica() {
 		document.getElementById('mosquito').remove()
 
 		if(vidas > 3) {
-			alert('Game Over')
+			window.location.href = 'fimDeJogo.html'
 		}
 		else {
 		document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
